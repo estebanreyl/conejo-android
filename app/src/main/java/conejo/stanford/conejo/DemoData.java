54530dafe34899c8b58e7932393501f390822a84
estebanreyl <erey@stanford.edu>
2017-11-30 21:06:31 -0800

    Added buttons for changing screens and improved view

--- app/src/main/java/conejo/stanford/conejo/DemoData.java
+++ app/src/main/java/conejo/stanford/conejo/DemoData.java
@@ -8,9 +8,11 @@ public class DemoData {
     public static int[] shirts = {R.mipmap.shirt_1,R.mipmap.shirt_2,R.mipmap.shirt_3,R.mipmap.shirt_4,
             R.mipmap.shirt_5,R.mipmap.shirt_6,R.mipmap.shirt_7};
 
-    public static int[] pants = {R.mipmap.pants_1,R.mipmap.pants_2,R.mipmap.pants_3,R.mipmap.pants_4};
+    public static int[] pants = {R.mipmap.pants_1,R.mipmap.pants_2,R.mipmap.pants_3,R.mipmap.pants_4,
+    R.mipmap.pants_5, R.mipmap.pants_6};
 
-    public static int[] shoes = {R.mipmap.shoe_1,R.mipmap.shoe_2,R.mipmap.shoe_3};
+    public static int[] shoes = {R.mipmap.shoe_1,R.mipmap.shoe_2,R.mipmap.shoe_3, R.mipmap.shoe_4,
+    R.mipmap.shoe_5,R.mipmap.shoe_6};
 
     public static int[] accessories = {R.mipmap.accesory_1,R.mipmap.accesory_2,R.mipmap.accesory_3};
 
