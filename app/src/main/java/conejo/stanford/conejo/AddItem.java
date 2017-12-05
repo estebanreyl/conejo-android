c630b2f362a072e80b2d255d8703843f7f02c18c
estebanreyl <erey@stanford.edu>
2017-12-04 23:38:14 -0800

    Added Profile activity

--- app/src/main/java/conejo/stanford/conejo/AddItem.java
+++ app/src/main/java/conejo/stanford/conejo/AddItem.java
@@ -25,9 +25,11 @@ public class AddItem extends AppCompatActivity {
         mCamera = getCameraInstance();
 
         // Create our Preview view and set it as the content of our activity.
+        mCamera.setDisplayOrientation(90);
         mPreview = new CameraPreview(this, mCamera);
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
         preview.addView(mPreview);
+
     }
 
     /** A safe way to get an instance of the Camera object. */
