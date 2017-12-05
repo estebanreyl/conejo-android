1db7978630577376e1ba0ce1e8b82a3668ab0e69
estebanreyl <erey@stanford.edu>
2017-12-05 01:58:11 -0800

    Finished up XML for profile activity

---
+++ app/src/main/java/conejo/stanford/conejo/PaymentInfo.java
@@ -0,0 +1,13 @@
+package conejo.stanford.conejo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PaymentInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_info);
+    }
+}
